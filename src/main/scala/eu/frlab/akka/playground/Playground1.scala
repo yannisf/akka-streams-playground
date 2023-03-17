0dd56a9e61efdb19655d64970f65c0a1cb87c75c
yannis <yanfrangos@pm.me>
2023-03-17 19:23:04 +0200

    Refactoring and resources

--- src/main/scala/eu/frlab/akka/playground/Playground1.scala
+++ src/main/scala/eu/frlab/akka/playground/Playground1.scala
@@ -11,6 +11,7 @@ object Playground1 extends App {
   implicit val system: ActorSystem = ActorSystem("QuickStart")
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
+  // https://doc.akka.io/docs/akka/current/stream/stream-quickstart.html
   val source: Source[Int, NotUsed] = Source(1 to 100)
   val done: Future[Done] = source.runForeach(i => println(i))
 
--- src/main/scala/eu/frlab/akka/playground/Playground2.scala
+++ src/main/scala/eu/frlab/akka/playground/Playground2.scala
@@ -17,18 +17,22 @@ object Playground2 extends App {
     Seq(Inner(3, b = false), Inner(4, b = false), Inner(5, b = false)),
   )
 
-  val source: Source[Outer, NotUsed] = Source.single(outer)
-
+  // https://doc.akka.io/docs/akka/current/stream/stream-graphs.html
   val graph: Graph[SourceShape[String], NotUsed] = GraphDSL.create() { implicit b =>
     import GraphDSL.Implicits._
+
+    val source: Source[Outer, NotUsed] = Source.single(outer)
 
     val bcast = b.add(Broadcast[Outer](2))
     val merge = b.add(Merge[String](2))
 
     source ~> bcast.in
 
-    bcast.out(0) ~> Flow[Outer].map(_.innerX).mapConcat(i => i.map(ii => s"${ii.i}${ii.b}")) ~> merge.in(0)
-    bcast.out(1) ~> Flow[Outer].map(_.innerY).mapConcat(i => i.map(ii => s"${ii.i}${ii.b}")) ~> merge.in(1)
+    val flowX: Flow[Outer, String, NotUsed] = Flow[Outer].map(_.innerX).mapConcat(i => i.map(ii => s"${ii.i}${ii.b}"))
+    val flowY: Flow[Outer, String, NotUsed] = Flow[Outer].map(_.innerY).mapConcat(i => i.map(ii => s"${ii.i}${ii.b}"))
+
+    bcast.out(0) ~> flowX ~> merge.in(0)
+    bcast.out(1) ~> flowY ~> merge.in(1)
 
     SourceShape(merge.out)
   }
